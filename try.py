b0e7128ac06e69b617d83ac2b82139f900ee82ba
Lukas Risian <240556@vut.cz>
2025-09-28 16:26:50 +0200

    Small changes (not important).

--- try.py
+++ try.py
@@ -4,9 +4,9 @@ from visualization import *
 
 
 # Path to nifti files
-image_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diplomová práca\Myel_001\ConvCT_data_nifti\myel_001_konv.nii.gz"
-lesions_mask_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diplomová práca\Myel_001\Lesion_labels\Myel_001_lesions_seg_validation_VV_final.nii.gz"
-vertebrae_mask_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diplomová práca\Myel_001\Spine_labels\myel_001_spine_seg_nnUNet_cor.nii.gz"
+image_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diploma thesis\Myel_001\ConvCT_data_nifti\myel_001_konv.nii.gz"
+lesions_mask_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diploma thesis\Myel_001\Lesion_labels\Myel_001_lesions_seg_validation_VV_final.nii.gz"
+vertebrae_mask_path = r"C:\Users\lukas\OneDrive - VUT\Plocha\Diploma thesis\Myel_001\Spine_labels\myel_001_spine_seg_nnUNet_cor.nii.gz"
 
 # Load the images to numpy_array
 ct_image = load_nifti_image(image_path)
@@ -15,13 +15,13 @@ vertebrae_mask = load_nifti_image(vertebrae_mask_path)
 
 # Vertebrae or lesions mask
 # lesion_voxels = (vertebrae_mask == 22) & (lesions_mask > 0)
-# vertebra_voxels = (vertebrae_mask == 22) & (lesions_mask == 0)
+# vertebra_voxels = (vertebrae_mask == 14) & (lesions_mask == 0)
 
 # Visualizations
 # show_individual_slices(ct_image)
 # show_slices_scroll(ct_image, axis=0, pause_time=0.01)
 # show_slices_scroll_with_mask(ct_image, vertebra_voxels, axis=0, pause_time=0.01)
-# show_slices_scroll_range_with_mask(ct_image, lesion_voxels, 0, 0.025, 200, 300)
+# show_slices_scroll_range_with_mask(ct_image, vertebra_voxels, start=150, end=300)
 
 # Datasets
 # create_features_dfs(ct_image, vertebrae_mask, lesions_mask)
