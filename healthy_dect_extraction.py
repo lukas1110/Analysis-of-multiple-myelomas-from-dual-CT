d2aefdf3c794a76f2c3b39b635d939efd3fe0a7d
Lukas <luki375.risian@gmail.com>
2026-04-04 18:50:31 +0200

    Plots with healthy vs MM tissue.

--- healthy_dect_extraction.py
+++ healthy_dect_extraction.py
@@ -139,6 +139,6 @@ class ExtractPatientsData:
 
 
 if __name__ == "__main__":
-    data_folder_path = r"E:\DATA_Healthy_and_Myelom\DATA_Healthy"
-    folder_names = "Healthy_"
+    data_folder_path = r"E:\DATA_Healthy_and_Myelom\DATA_MM_Stage3"
+    folder_names = "Myel_"
     ExtractPatientsData(data_folder_path, folder_names).process_all_patients()
